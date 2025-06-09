d88bc0f16d0684c9f96f4b133bdfa19caa3b16a5
kk3L <kielongtengco@gmail.com>
2025-06-09 19:42:27 +0800

    1

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
               <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/resume">Resume</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/contact">Contacts</Link></li>
               </ul>
             </div>
           </div>
